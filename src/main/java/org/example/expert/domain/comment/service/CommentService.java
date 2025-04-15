abdfe79f6b305e05d46c34f86e294a3bfb1fc468
sukh115 <sukh1151102@gmail.com>
2025-04-15 20:25:29 +0900

    refector : comment 리팩토링

--- src/main/java/org/example/expert/domain/comment/service/CommentService.java
+++ src/main/java/org/example/expert/domain/comment/service/CommentService.java
@@ -28,8 +28,7 @@ public class CommentService {
     @Transactional
     public CommentSaveResponse saveComment(AuthUser authUser, long todoId, CommentSaveRequest commentSaveRequest) {
         User user = User.fromAuthUser(authUser);
-        Todo todo = todoRepository.findById(todoId).orElseThrow(() ->
-                new InvalidRequestException("Todo not found"));
+        Todo todo = todoRepository.findByIdOrThrow(todoId);
 
         Comment newComment = new Comment(
                 commentSaveRequest.getContents(),
